f922b4ed8fc2afce29f856e7dab9a73782e19ebb
oceylantr <oceylantr@gmail.com>
2022-05-27 09:27:41 +0300

    Delete BankDemo directory

--- BankDemo/src/main/java/com/example/demo/services/interfaces/IAccountQueryService.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.services.interfaces;
-
-import java.util.List;
-
-public interface IAccountQueryService {
-	
-	public List<Object> listEventsForAccount(String accountNo);
-	
-}
